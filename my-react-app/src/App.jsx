bb859d198b3739b87e330fdf581260780d4eae65
Rubin <rslukongo@student.apc.edu.ph>
2026-02-21 12:59:48 +0000

    adjusted my navbar to be in all pages

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Book from "./pages/Book";
 import Gallery from "./pages/Gallery";
@@ -6,12 +8,16 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/book" element={<Book />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
